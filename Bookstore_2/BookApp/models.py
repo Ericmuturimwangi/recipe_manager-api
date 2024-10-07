e5287a682aa269b61c1154faba3713735bcca364
Eric <muturiwamwanginanyawira@gmail.com>
2024-10-07 20:00:01 +0300

    Delete Bookstore_2 directory

--- Bookstore_2/BookApp/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-
-class BookModel(models.Model):
-    name = models.CharField(max_length=100)
-    author = models.CharField(max_length=100)
--- Bookstore_2/BookApp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .api import BookListApi, BookCreateApi
-
-urlpatterns = [
-    path("list", BookListApi),
-    path("create", BookCreateApi),
-]
