b43ea8f57b8ec1b1fd9ba741eedbfb770f38f480
Eric <muturiwamwanginanyawira@gmail.com>
2024-10-07 20:00:53 +0300

    Delete refresher directory

--- refresher/roadmap/mysite/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path("polls/", include("polls.urls")),
-    path("admin/", admin.site.urls),
-]
--- refresher/src/demo/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Car, Manufacturer
-
-admin.site.register(Car)
-admin.site.register(Manufacturer)
