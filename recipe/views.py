aeacc19c132e0c853fb1ad98084484606a8a02b0
EricMuturiMwangi <muturiwamwanginanyawira@gmail.com>
2024-10-07 21:16:38 +0300

    Add role based access control

--- recipe/views.py
+++ recipe/views.py
@@ -5,7 +5,8 @@ from rest_framework import viewsets
 from rest_framework import filters
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.permissions import IsAuthenticated
-
+from .permissions import IsAdminOrOwner
+from rest_framework.response import Response
 class RecipeViewSet(viewsets.ModelViewSet):
     queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
@@ -13,7 +14,21 @@ class RecipeViewSet(viewsets.ModelViewSet):
     filterset_fields = ['ingredients', 'category']
     search_fields = ['title']
     ordering_fields = ['created_at', 'updated_at']
-    permission_classess= [IsAuthenticated]
+    permission_classes= [IsAuthenticated, IsAdminOrOwner]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user) #check whether the user is the owner of the recipe
+
+    def destroy(self, request, *args, **kwargs):
+        # only admins and owners can delete the recipes
+        recipe = self.get_object()
+        if request.user != recipe.owner and not request.user.groups.filter(name='Admin').exists():
+            return Response({'error':'You do not have the permission to del this recipe'}, status=403)
+        
+        return super().destroy(request, *args, **kwargs)
+
+
+
 
 
 
